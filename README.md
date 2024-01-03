1cc671d7676976948518359f896c852960f328a7
Wilder Lopes <wilder@ogre.run>
2024-01-03 10:33:53 -0600

    First comit

---
+++ README.md
@@ -0,0 +1,7 @@
+# To test ogre github app
+
+Make sure to install the following packages:
+
+- pytorch
+- matplotlib
+- streamlit
---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit
+import torch
+import numpy
+import matplotlib.pyplot as plt
+
+
+print(streamlit.__version__())
+
